79ed753b05971eb0b5005f851cf7d99d3382780e
Roma <r.bogdan80@gmail.com>
2020-07-26 17:22:02 +0300

    Створено frontend\form\RegistrationForm та перевизначено Da\User\Form\RegistrationForm

---
+++ frontend/form/RegistrationForm.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace frontend\form;
+
+use Da\User\Form\RegistrationForm as BaseForm;
+
+class RegistrationForm extends BaseForm {
+
+    public function rules() {
+            return [
+                [['reCaptcha'], \himiklab\yii2\recaptcha\ReCaptchaValidator3::className(),
+                    'threshold' => 0.5,
+                    'action' => 'homepage',
+                ],
+            ];
+        }
+
+}
