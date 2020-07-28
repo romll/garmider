0f45d3a3c4eaa4efcedff9d0a29ded3aa8161db1
Roma <r.bogdan80@gmail.com>
2020-07-29 00:03:28 +0300

    виправлено помилку в RegistrationForm

--- frontend/form/RegistrationForm.php
+++ frontend/form/RegistrationForm.php
@@ -6,6 +6,8 @@ use Da\User\Form\RegistrationForm as BaseForm;
 
 class RegistrationForm extends BaseForm {
 
+    public $reCaptcha;
+
     public function rules() {
             return [
                 [['reCaptcha'], \himiklab\yii2\recaptcha\ReCaptchaValidator3::className(),
